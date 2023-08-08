27d3f51d64f2d83c459450d2df3d32de12c91240
gabrielsimaoamaral <135365686+gabrielsimaoamaral@users.noreply.github.com>
2023-08-08 19:31:28 -0300

    Projeto final do curso

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,31 @@
+import Banner from 'components/Banner'
+import Card from 'components/Card'
+import Titulo from 'components/Titulo'
+import React, { useEffect, useState } from 'react'
+import styles from './Inicio.module.css';
+
+export default function Inicio() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/gabrielsimaoamaral/cinetag-api/videos')
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setVideos(dados)
+      })
+  }, [])
+
+  return (
+    <>
+      <Banner imagem="home" />
+      <Titulo>
+        <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+      </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />
+        })}
+      </section>
+    </>
+  )
+}
